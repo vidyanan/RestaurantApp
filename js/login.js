95af3bd0f37b80d7b0cddd55f6132832165f41d6
Omar Chehab <omarchehab98@gmail.com>
2019-03-01 17:40:29 -0500

    Fix links in navigation and login

--- js/login.js
+++ js/login.js
@@ -4,13 +4,13 @@ loginForm.addEventListener('submit', submitLoginForm);
 
 function submitLoginForm(e) {
 	e.preventDefault();
-	
+
 	const username = loginForm.username.value;
 	const password = loginForm.password.value;
-	
+
 	// add backend login to encrtpt password save and verify type of account
 	if (username=="admin" && password=="admin"){
-		window.location.href = 'admin.html';
+		window.location.href = 'adminPage.html';
 	} else if (username=="user" && password=="user"){
 		window.location.href = 'restaurant.html';
 	} else if (username=="user2" && password=="user2"){
@@ -18,5 +18,5 @@ function submitLoginForm(e) {
 	} else {
 		alert("error: invalid username and password")
 	}
-	
+
 }
