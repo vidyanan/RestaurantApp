f924c1a25c7638edd04906eb053663c13a08f005
ninjayoshi <tim470773@gmail.com>
2019-03-01 15:41:04 -0500

    Added server hardcoding.

--- js/restaurantOwner.js
+++ js/restaurantOwner.js
@@ -1,3 +1,28 @@
+// // // // // // // // // // // // // // // // // //
+// TEMPORARY SERVER/DATABASE VARIABLE FOR PHASE 1 //
+// // // // // // // // // // // // // // // // // //
+let yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+yesterday = formatDate(yesterday);
+let today = new Date();
+today = formatDate(today);
+let tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+tomorrow = formatDate(tomorrow);
+
+let server = {};
+server[yesterday] = [{"id": 1, "table": 0, "host": "Bob", "hour": 5, "timeSlot": 30}];
+server[today] = [{"id": 0, "table": 0, "host": "Tim", "hour": 3, "timeSlot": 0}, {"id": 2, "table": 3, "host": "Jim", "hour": 3, "timeSlot": 30}];
+server[tomorrow] = [{"id": 3, "table": 5, "host": "Him", "hour": 3, "timeSlot": 0}];
+
+let serverMaxReservations = 5;
+let serverNextID = 4;
+
+
+
+let maxReservations;
+let currentDate = new Date();
+
 const reservationList = document.querySelector('#reservationList');
 const addReservationForm = document.querySelector('#addReservationForm');
 const maxReservationForm = document.querySelector('#maxReservationForm');
@@ -5,12 +30,9 @@ const previousDateButton = document.querySelector('#previousDate');
 const nextDateButton = document.querySelector('#nextDate');
 const currentDateText = document.querySelector('#currentDate');
 
-let maxReservations;
-let currentDate = new Date();
-
-reservationList.addEventListener('click', removeReservation);
+reservationList.addEventListener('click', removeButtonFunction);
 addReservationForm.addEventListener('submit', submitReservationForm);
-maxReservationForm.addEventListener('submit', updateMaxReservations);
+maxReservationForm.addEventListener('click', updateMaxReservations);
 previousDateButton.addEventListener('click', getPreviousDay);
 nextDateButton.addEventListener('click', getNextDay);
 
@@ -27,13 +49,22 @@ function fitTimeSlot(date) {
 
 // Checks if there are too many reservations for another at time date
 function doesConflict(date) {
-  let day = date.getDay();
-  let timeSlot = date.getTime();
+  let hour = date.getHours();
+  let timeSlot = date.getMinutes();
 
-  // Query database to get count of reservations at that time slot on that day
   let reservCount = 0;
 
-  return reservCount + 1 < maxReservations;
+  // Query database to get count of reservations at that time slot on that day
+  // {
+      let serverLst = server[formatDate(date)];
+      for(let i = 0; i < serverLst.length; i++) {
+        if(serverLst[i]['hour'] === hour || serverLst[i]['timeSlot'] === timeSlot) {
+          reservCount++;
+        }
+      }
+  // }
+
+  return reservCount > maxReservations;
 }
 
 // Checks if date is the currently displayed date
@@ -55,9 +86,13 @@ function getPreviousDay() {
   createCurrentDayReservations();
 }
 
+function formatDate(date) {
+  return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+}
+
 // Changes the html date on the page
 function updateTextDate() {
-  currentDateText.innerText = (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear();
+  currentDateText.innerText = formatDate(currentDate);
 }
 
 // // // // // // // // //
@@ -81,8 +116,14 @@ function submitReservationForm(e) {
 
 // Get the lowest new reservation id
 function getNewReservationId() {
+  let newId;
+
   // Call database
-  return 1;
+  // {
+    newId = serverNextID;
+    serverNextID++;
+  // }
+  return newId;
 }
 
 // Updates the maximum reservations allowed in a time slot
@@ -93,16 +134,35 @@ function updateMaxReservations(e) {
   maxReservations = newMax;
 
   // Call to database to update the max
+  serverMaxReservations = maxReservations;
 }
 
 // Only called on startup
 function retrieveMaxReservations() {
-  maxReservations = 5; // Call to database to find out the actual max
+  maxReservations = serverMaxReservations; // Call to database to find out the actual max
 }
 
 function getFreeTable(date) {
+  let table = 0;
+
   // Query database to return the first table free at date
-  return 1
+  //{
+    // This function just goes through the dictionary in server until an incremented int doesn't appear for that date
+    let list = server[formatDate(date)];
+    let tempTable = 1;
+    while(table === 0) {
+      for (let i = 0; i < list.length; i++) {
+        if(list[i]['table'] === tempTable) {
+          break;
+        } else if (i === list.length - 1) {
+          table = tempTable;
+        }
+      }
+      tempTable++;
+    }
+  //}
+
+  return table;
 }
 
 // // // // // // // // // // // // //
@@ -112,9 +172,24 @@ function getFreeTable(date) {
 // Creates a new reservation on reservationList
 function createNewReservation(hostName, reservationDate) {
   let tableNum = getFreeTable(reservationDate);
+  let id = getNewReservationId();
 
   // Add new entry into database
+  // {
+      for(let i = 0; i < Object.keys(server).length; i++) {
+        if (formatDate(reservationDate) === Object.keys(server)[i]) {
+          server[formatDate(reservationDate)].push({'id': id, 'table': tableNum, 'host': hostName, 'hour': reservationDate.getHours(), 'timeSlot': reservationDate.getMinutes()});
+          break;
+        } else if (i === Object.keys(server).length - 1) {
+          server[formatDate(reservationDate)] = [{'id': id, 'table': tableNum, 'host': hostName, 'hour': reservationDate.getHours(), 'timeSlot': reservationDate.getMinutes()}];
+        }
+      }
+  // }
 
+  addReservation(hostName, reservationDate, tableNum, id)
+}
+
+function addReservation(hostName, reservationDate, tableNum, id) {
   let newReservation = document.createElement('div');
   newReservation.setAttribute('class', 'row');
 
@@ -122,7 +197,7 @@ function createNewReservation(hostName, reservationDate) {
   let reservationInfo = document.createElement('div');
   reservationInfo.setAttribute('class', 'col');
 
-  reservationInfo.innerText = getNewReservationId();
+  reservationInfo.innerText = id;
   newReservation.appendChild(reservationInfo);
 
   reservationInfo = reservationInfo.cloneNode(false);
@@ -145,11 +220,37 @@ function createNewReservation(hostName, reservationDate) {
   reservationList.appendChild(newReservation);
 }
 
-// Remove reservation from reservationList
-function removeReservation(e) {
-  reservationList.removeChild(e.target.parentNode);
+// Occurs when the remove button on a reservation occurs
+function removeButtonFunction(e) {
+  e.preventDefault();
+
+  if (e.target.getAttribute('class') === "removeReservation") {
+    deleteReservation(e.target.parentNode);
+  }
 }
 
+// Remove reservation from reservationList
+function deleteReservation(child) {
+  reservationList.removeChild(child);
+
+  // Remove entry from database
+  // {
+      // irl it would go by id
+      let id = parseInt(child.firstChild.innerText);
+      let date = new Date(child.firstChild.nextSibling.nextSibling.nextSibling.innerText);
+      let serverLst = server[formatDate(date)];
+
+      for(let i = 0; i < serverLst.length; i++) {
+        if(serverLst[i]['id'] === id) {
+          serverLst.splice(i, 1);
+          server[formatDate(date)] = serverLst;
+          break;
+        }
+      }
+  // }
+}
+
+// Removes but doesn't delete all reservations
 function removeAllReservations() {
   let header = reservationList.firstElementChild;
 
@@ -165,16 +266,21 @@ function removeAllReservations() {
 
 // Puts reservations on reservationList for all entries on date
 function createDayReservations() {
-  let dates = [];
+  let dates;
 
   // Call database to get all dates on date
-
-  dates.sort();
+  // {
+      dates = server[formatDate(currentDate)];
+      // sort in sql
+  // }
 
   removeAllReservations();
 
+  let tempDate = currentDate;
   for (let i = 0; i < dates.length; i++) {
-    createNewReservation(dates[i]);
+    tempDate.setHours(dates[i]['hour']);
+    tempDate.setMinutes(dates[i]['timeSlot']);
+    addReservation(dates[i]['host'], tempDate, dates[i]['table'], dates[i]['id']);
   }
 }
 
@@ -185,5 +291,10 @@ function createCurrentDayReservations() {
   updateTextDate();
 }
 
-window.onload=retrieveMaxReservations;
-window.onload=createCurrentDayReservations;
+// Run startup procedures
+function initialize() {
+  retrieveMaxReservations();
+  createCurrentDayReservations();
+}
+
+window.onload=initialize;
