744efd73b18299b5ad47af3bef4db6df7c5019c3
iman kewalramani <iman.kumarkewalramani@mail.utoronto.ca>
2019-03-01 20:50:17 -0500

    fixed

--- js/restEdit.js
+++ js/restEdit.js
@@ -1,18 +1,19 @@
 
 const myform = document.querySelector('#profileForm');
-console.log(myform)
-var x = document.getElementById("myRadio");
-var v = document.getElementById("myRadio1");
-console.log(x.checked);
-console.log(v.checked);
-console.log(window.location.href.split("?"))
+myform.addEventListener('click', beginClosing);
+
+
 fillInData(window.location.href.split("?")[1])
-function closeForm() {
-  window.close()
-}
+
 
 function fillInData(i){
-
   myform.children[0].value = i;
+  //get restaurant info for restaruant id i
+}
 
+function beginClosing(e){
+  if (e.target.classList.contains('btn modify')) {
+    //Push changes to server
+  }
+  window.close();
 }
--- js/userEdit.js
+++ js/userEdit.js
@@ -1,33 +1,22 @@
-
 const myform = document.querySelector('#profileForm');
-myform.addEventListener('click', beginClosing);
+myform.addEventListener('submit', beginClosing);
 
-console.log(myform)
-var x = document.getElementById("myRadio");
-var v = document.getElementById("myRadio1");
-console.log(x.checked);
-console.log(v.checked);
-console.log(window.location.href.split("?"))
+// var x = document.getElementById("myRadio");
+// var v = document.getElementById("myRadio1");
+// console.log(x.checked);
+// console.log(v.checked);
+// console.log(window.location.href.split("?"))
+
 fillInData(window.location.href.split("?")[1])
-function closeForm() {
-  window.close()
-}
 
 function fillInData(i){
   myform.children[0].value = i;
+  //get data on user i from database
 }
 
 function beginClosing(e){
-  if (e.target.classList.contains('btn exit')) {
-    window.close;
-
-  }
-  if (e.target.classList.contains('btn cancel')) {
-    window.close;
-  }
   if (e.target.classList.contains('btn modify')) {
-
-    window.close;
+    //Push changes to server
   }
-
+  window.close();
 }
