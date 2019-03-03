8e9aa8cee576d801b088ea0dd8f2783ebaabf4a3
Omar Chehab <omarchehab98@gmail.com>
2019-03-02 19:04:15 -0500

    Restore custom components file that Iman deleted

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export * from '/components/Navigation/Navigation.js';
+export * from '/components/CoverCard/CoverCard.js';
+export * from '/components/CoverCurve/CoverCurve.js';
+export * from '/components/CoverImage/CoverImage.js';
+export * from '/components/RestaurantPreview/RestaurantPreview.js';
