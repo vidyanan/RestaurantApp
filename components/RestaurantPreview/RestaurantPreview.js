cc7b1a69b79c947eac2e2f29713da7311c654a6e
Omar Chehab <omarchehab98@gmail.com>
2019-03-02 19:09:47 -0500

    Improve styling of home page

--- components/RestaurantPreview/RestaurantPreview.js
+++ components/RestaurantPreview/RestaurantPreview.js
@@ -6,7 +6,7 @@ export class RestaurantPreview extends HTMLElement {
     const fragment = document.importNode(template.content, true)
     shadowRoot.appendChild(fragment)
 
-    $(shadowRoot).find('.title').text(this.getAttribute('title'));
+    $(shadowRoot).find('.heading').text(this.getAttribute('heading'));
     $(shadowRoot).find('.image').attr('src', this.getAttribute('image'));
     $(shadowRoot).find('.url').attr('href', this.getAttribute('url'));
   }
