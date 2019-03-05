f9c400c4c8e63c9c3d600ae53f9eb880d09cc53c
Omar Chehab <omarchehab98@gmail.com>
2019-03-04 23:46:46 -0500

    Add loggedin attribute to navigation component

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,8 +1,37 @@
 export class Navigation extends HTMLElement {
+  static get observedAttributes() {
+    return Object.keys(this.onAttributeChanged);
+  }
+
+  static get onAttributeChanged() {
+    return {
+      loggedin: Navigation.prototype.onLoggedinAttributeChanged,
+    }
+  }
+
   constructor() {
     super();
     const template = document.getElementById("Navigation");
     const fragment = document.importNode(template.content, true);
     this.appendChild(fragment);
+
+    this.$loggedout = $('.navigation-loggedout');
+    this.$loggedin = $('.navigation-loggedin');
+    this.$loggedin.css('display', 'none');
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    const handler = Navigation.onAttributeChanged[attrName];
+    handler.call(this, oldVal, newVal);
+  }
+
+  onLoggedinAttributeChanged(oldVal, newVal) {
+    if (newVal !== null) {
+      this.$loggedout.css('display', 'none');
+      this.$loggedin.css('display', '');
+    } else {
+      this.$loggedout.css('display', '');
+      this.$loggedin.css('display', 'none');
+    }
   }
 }
