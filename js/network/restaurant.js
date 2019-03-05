1e74088849ab725d1e2a75ae916114b4de6e5431
Omar Chehab <omarchehab98@gmail.com>
2019-03-05 14:39:34 -0500

    Refactor restaurantsSearch page

--- js/network/restaurant.js
+++ js/network/restaurant.js
@@ -1,9 +1,268 @@
 export {
+  getRestaurants,
+  getRestaurantLocations,
+  getRestaurantCuisines,
   getRestaurantBySlug,
   getRestaurantReviewsByRestaurantSlug,
   createRestaurantBooking,
   createRestaurantReview,
 };
+
+/**
+ * Fetches restaurants that match the filters, returns a promise that resolves to an array of
+ * restaurants.
+ *
+ * @param {Object} filter
+ * @param {String} filter.location
+ * @param {String} filter.cuisine
+ * @param {Date|String|Number} filter.datetime
+ * @returns {Promise<Array<Object>>}
+ * @example
+ * await getRestaurants()
+ * [
+ *   {
+ *     name: "Porta Via",
+ *     featuredImage: "/images/restaurants/porta-via.png",
+ *     url: "/restaurant.html?id=porta-via",
+ *     location: "Mississauga",
+ *     cuisine: "Mexican",
+ *     hours: [
+ *       [540, 1320],
+ *       [540, 1320],
+ *       [540, 1320],
+ *       [540, 1320],
+ *       [540, 1320],
+ *       [540, 1440],
+ *       [540, 1440],
+ *     ],
+ *   }
+ * ]
+ */
+async function getRestaurants({
+  location = null,
+  cuisine = null,
+  datetime = null,
+}={}) {
+  const byLocation = location && location.length >= 3;
+  const byCuisine = cuisine && cuisine.length >= 3;
+  const byHours = datetime;
+  const date = new Date(datetime);
+  if (byHours && date < new Date()) {
+    return [];
+  }
+  return [{
+    name: "Porta Via",
+    featuredImage: "/images/restaurants/porta-via.png",
+    url: "/restaurant.html?id=porta-via",
+    location: "Mississauga",
+    cuisine: "Mexican",
+    hours: [
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1440],
+      [540, 1440],
+    ],
+  }, {
+    name: "Flava Ceen",
+    featuredImage: "/images/restaurants/flava-ceen.jpg",
+    url: "/restaurant.html?id=flava-ceen",
+    location: "Mississauga",
+    cuisine: "Jamaican",
+    hours: [
+      [540, 60],
+      [540, 60],
+      [540, 60],
+      [540, 60],
+      [540, 60],
+      [540, 60],
+      [540, 60],
+    ],
+  }, {
+    name: "Umami Hakka",
+    featuredImage: "/images/restaurants/umami-hakka.jpg",
+    url: "/restaurant.html?id=umami-hakka",
+    location: "Mississauga",
+    cuisine: "Indian",
+    hours: [
+      null,
+      null,
+      null,
+      null,
+      null,
+      [1020, 1380],
+      [1020, 1380],
+    ],
+  }, {
+    name: "Swiss Chalet",
+    featuredImage: "/images/restaurants/swiss-chalet.jpg",
+    url: "/restaurant.html?id=swiss-chalet",
+    location: "Mississauga",
+    cuisine: "Canadian",
+    hours: [
+      [780, 1320],
+      [780, 1320],
+      [780, 1320],
+      [780, 1320],
+      [780, 1320],
+      [780, 1320],
+      [780, 1320],
+    ],
+  }, {
+    name: "Sail Sushi",
+    featuredImage: "/images/restaurants/sail-sushi.jpg",
+    url: "/restaurant.html?id=sail-sushi",
+    location: "Toronto",
+    cuisine: "Japanese",
+    hours: [
+      [1080, 120],
+      [1080, 120],
+      [1080, 120],
+      [1080, 120],
+      [1080, 120],
+      [1080, 120],
+      [1080, 120],
+    ],
+  }, {
+    name: "Big Bite Burrito",
+    featuredImage: "/images/restaurants/big-bite-burrito.jpg",
+    url: "/restaurant.html?id=big-bite-burrito",
+    location: "Scarborough",
+    cuisine: "Mexican",
+    hours: [
+      [660, 1320],
+      [660, 1320],
+      [660, 1320],
+      [660, 1320],
+      [660, 1320],
+      [660, 1320],
+      [660, 1320],
+    ],
+  }, {
+    name: "Twice As Nice",
+    featuredImage: "/images/restaurants/twice-as-nice.jpg",
+    url: "/restaurant.html?id=twice-as-nice",
+    location: "Toronto",
+    cuisine: "Jamaican",
+    hours: [
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+    ],
+  }, {
+    name: "Sunrise Caribbean",
+    featuredImage: "/images/restaurants/sunrise-caribbean.jpg",
+    url: "/restaurant.html?id=sunrise-caribbean",
+    location: "Toronto",
+    cuisine: "Jamaican",
+    hours: [
+      [660, 1200],
+      [660, 1200],
+      [660, 1200],
+      [660, 1200],
+      [660, 1200],
+      [660, 1200],
+      [660, 1200],
+    ],
+  }, {
+    name: "Kinton Ramen",
+    featuredImage: "/images/restaurants/kinton-ramen.jpg",
+    url: "/restaurant.html?id=kinton-ramen",
+    location: "Scarborough",
+    cuisine: "Japanese",
+    hours: [
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+    ],
+  }, {
+    name: "Little Caesars",
+    featuredImage: "/images/restaurants/little-caesars.jpg",
+    url: "/restaurant.html?id=little-caesars",
+    location: "Scarborough",
+    cuisine: "Italian",
+    hours: [
+      [540, 60],
+      [540, 60],
+      [540, 60],
+      [540, 60],
+      [540, 60],
+      [660, 180],
+      [660, 180],
+    ],
+  }, {
+    name: "La Sani Grill",
+    featuredImage: "/images/restaurants/la-sani-grill.jpg",
+    url: "/restaurant.html?id=la-sani-grill",
+    location: "Scarborough",
+    cuisine: "Mediterranean",
+    hours: [
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+      [540, 1320],
+    ],
+  }].filter((x) => {
+    return (
+      (!byLocation || fuzzyFilter(location, x.location)) &&
+      (!byCuisine || fuzzyFilter(cuisine, x.cuisine)) &&
+      (!byHours || inHours(x.hours, date))
+    )
+  });
+}
+
+/**
+ * Fetches restaurant locations, returns a promise that resolves to an array of locations.
+ *
+ * @returns {Promise<Array<String>>}
+ * @example
+ * await getRestaurantLocations()
+ * [
+ *   "Mississauga",
+ * ]
+ */
+async function getRestaurantLocations() {
+  return [
+    "Mississauga",
+    "Scarborough",
+    "Toronto",
+  ];
+}
+
+/**
+ * Fetches restaurant cuisines, returns a promise that resolves to an array of cuisines.
+ *
+ * @returns {Promise<Array<String>>}
+ * @example
+ * await getRestaurantCuisines()
+ * [
+ *   "Mississauga",
+ * ]
+ */
+async function getRestaurantCuisines() {
+  return [
+    "Canadian",
+    "Indian",
+    "Italian",
+    "Jamaican",
+    "Japanese",
+    "Mediterranean",
+    "Mexican",
+  ];
+}
 
 /**
  * Fetches a restaurant by its id, returns a promise that resolves to a restaurant.
@@ -115,4 +374,38 @@ async function createRestaurantBooking(booking) {
  */
 async function createRestaurantReview(review) {
   return null;
+}
+
+
+
+// Helper functions that should be on the server
+function fuzzyFilter(searchText, key) {
+  if (!searchText || !key) {
+    return;
+  }
+  const compareString = key.toLowerCase();
+  searchText = searchText.toLowerCase();
+
+  let searchTextIndex = 0;
+  for (let index = 0; index < key.length; index++) {
+    if (compareString[index] === searchText[searchTextIndex]) {
+      searchTextIndex += 1;
+    }
+  }
+
+  return searchTextIndex === searchText.length;
+}
+
+function inHours(hours, date) {
+  const d = date.getDay();
+  const h = date.getHours();
+  const m = date.getMinutes();
+  if (!hours[d]) {
+    return false;
+  }
+  const [ from, to ] = hours[d];
+  const x = (h * 60) + m;
+  return from === to ||
+    (from < to && from < x && x < to) ||
+    (from > to && !(to < x && x < from));
 }
--- js/restaurantsSearch.js
+++ js/restaurantsSearch.js
@@ -1,188 +1,27 @@
-// Hardcoded data that should be in the database
-const restaurantData = [{
-  name: "Porta Via",
-  featuredImage: "/images/restaurants/porta-via.png",
-  url: "/restaurant.html?id=porta-via",
-  location: "Mississauga",
-  cuisine: "Mexican",
-  hours: [
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1440],
-    [540, 1440],
-  ],
-}, {
-  name: "Flava Ceen",
-  featuredImage: "/images/restaurants/flava-ceen.jpg",
-  url: "/restaurant.html?id=flava-ceen",
-  location: "Mississauga",
-  cuisine: "Jamaican",
-  hours: [
-    [540, 60],
-    [540, 60],
-    [540, 60],
-    [540, 60],
-    [540, 60],
-    [540, 60],
-    [540, 60],
-  ],
-}, {
-  name: "Umami Hakka",
-  featuredImage: "/images/restaurants/umami-hakka.jpg",
-  url: "/restaurant.html?id=umami-hakka",
-  location: "Mississauga",
-  cuisine: "Indian",
-  hours: [
-    null,
-    null,
-    null,
-    null,
-    null,
-    [1020, 1380],
-    [1020, 1380],
-  ],
-}, {
-  name: "Swiss Chalet",
-  featuredImage: "/images/restaurants/swiss-chalet.jpg",
-  url: "/restaurant.html?id=swiss-chalet",
-  location: "Mississauga",
-  cuisine: "Canadian",
-  hours: [
-    [780, 1320],
-    [780, 1320],
-    [780, 1320],
-    [780, 1320],
-    [780, 1320],
-    [780, 1320],
-    [780, 1320],
-  ],
-}, {
-  name: "Sail Sushi",
-  featuredImage: "/images/restaurants/sail-sushi.jpg",
-  url: "/restaurant.html?id=sail-sushi",
-  location: "Toronto",
-  cuisine: "Japanese",
-  hours: [
-    [1080, 120],
-    [1080, 120],
-    [1080, 120],
-    [1080, 120],
-    [1080, 120],
-    [1080, 120],
-    [1080, 120],
-  ],
-}, {
-  name: "Big Bite Burrito",
-  featuredImage: "/images/restaurants/big-bite-burrito.jpg",
-  url: "/restaurant.html?id=big-bite-burrito",
-  location: "Scarborough",
-  cuisine: "Mexican",
-  hours: [
-    [660, 1320],
-    [660, 1320],
-    [660, 1320],
-    [660, 1320],
-    [660, 1320],
-    [660, 1320],
-    [660, 1320],
-  ],
-}, {
-  name: "Twice As Nice",
-  featuredImage: "/images/restaurants/twice-as-nice.jpg",
-  url: "/restaurant.html?id=twice-as-nice",
-  location: "Toronto",
-  cuisine: "Jamaican",
-  hours: [
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-  ],
-}, {
-  name: "Sunrise Caribbean",
-  featuredImage: "/images/restaurants/sunrise-caribbean.jpg",
-  url: "/restaurant.html?id=sunrise-caribbean",
-  location: "Toronto",
-  cuisine: "Jamaican",
-  hours: [
-    [660, 1200],
-    [660, 1200],
-    [660, 1200],
-    [660, 1200],
-    [660, 1200],
-    [660, 1200],
-    [660, 1200],
-  ],
-}, {
-  name: "Kinton Ramen",
-  featuredImage: "/images/restaurants/kinton-ramen.jpg",
-  url: "/restaurant.html?id=kinton-ramen",
-  location: "Scarborough",
-  cuisine: "Japanese",
-  hours: [
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-  ],
-}, {
-  name: "Little Caesars",
-  featuredImage: "/images/restaurants/little-caesars.jpg",
-  url: "/restaurant.html?id=little-caesars",
-  location: "Scarborough",
-  cuisine: "Italian",
-  hours: [
-    [540, 60],
-    [540, 60],
-    [540, 60],
-    [540, 60],
-    [540, 60],
-    [660, 180],
-    [660, 180],
-  ],
-}, {
-  name: "La Sani Grill",
-  featuredImage: "/images/restaurants/la-sani-grill.jpg",
-  url: "/restaurant.html?id=la-sani-grill",
-  location: "Scarborough",
-  cuisine: "Mediterranean",
-  hours: [
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-    [540, 1320],
-  ],
-}];
-const locationData = Array.from(new Set(restaurantData.map(x => x.location)));
-const cuisineData = Array.from(new Set(restaurantData.map(x => x.cuisine)));
+import {
+  getRestaurants,
+  getRestaurantLocations,
+  getRestaurantCuisines
+} from '/js/network/index.js';
 
 window.addEventListener('CSC309CustomElementsReady', () => {
-  $('#location').on('input', () => getRestaurants(getFilters()).then(renderRestaurants))
-  $('#cuisine').on('input', () => getRestaurants(getFilters()).then(renderRestaurants))
-  $('#datetime').on('input', () => getRestaurants(getFilters()).then(renderRestaurants))
-  populateDatalist($('#locations'), locationData);
-  populateDatalist($('#cuisines'), cuisineData);
-  setFilters(new URLSearchParams(location.search))
-  getRestaurants(getFilters()).then(renderRestaurants)
-});
+  $('#location').on('input', () => getRestaurants(getFilters()).then(renderRestaurants));
+  $('#cuisine').on('input', () => getRestaurants(getFilters()).then(renderRestaurants));
+  $('#datetime').on('input', () => getRestaurants(getFilters()).then(renderRestaurants));
+  getRestaurants(getFilters()).then(renderRestaurants);
+  getRestaurantLocations().then((locationData) =>
+    $('#locations').append(locationData.map(item => $('<option></option>').text(item)))
+  );
+  getRestaurantCuisines().then((cuisineData) =>
+    $('#cuisines').append(cuisineData.map(item => $('<option></option>').text(item)))
+  );
 
-function setFilters(query) {
+  const query = new URLSearchParams(location.search);
+
   $('#location').attr('value', query.get('location'));
   $('#cuisine').attr('value', query.get('cuisine'));
   $('#datetime').attr('value', query.get('datetime'));
-}
+});
 
 function getFilters() {
   return {
@@ -193,9 +32,9 @@ function getFilters() {
 }
 
 function renderRestaurants(results) {
-  const $restaurants = $('#restaurants');
-  $restaurants.empty();
-  $restaurants.append(results.map(renderRestaurantPreview));
+  $('#restaurants')
+    .empty()
+    .append(results.map(renderRestaurantPreview));
 }
 
 function renderRestaurantPreview(x) {
@@ -203,61 +42,4 @@ function renderRestaurantPreview(x) {
       .attr('heading', x.name)
       .attr('image', x.featuredImage)
       .attr('url', x.url)
-}
-
-  // GET data from server
-async function getRestaurants({
-  location = null,
-  cuisine = null,
-  datetime = null,
-}={}) {
-  const byLocation = location && location.length >= 3;
-  const byCuisine = cuisine && cuisine.length >= 3;
-  const byHours = datetime;
-  const date = new Date(datetime);
-  if (byHours && date < new Date()) {
-    return [];
-  }
-  return restaurantData.filter((x) => {
-    return (
-      (!byLocation || fuzzyFilter(location, x.location)) &&
-      (!byCuisine || fuzzyFilter(cuisine, x.cuisine)) &&
-      (!byHours || inHours(x.hours, date))
-    )
-  });
-}
-
-function fuzzyFilter(searchText, key) {
-  if (!searchText || !key) {
-    return;
-  }
-  const compareString = key.toLowerCase();
-  searchText = searchText.toLowerCase();
-
-  let searchTextIndex = 0;
-  for (let index = 0; index < key.length; index++) {
-    if (compareString[index] === searchText[searchTextIndex]) {
-      searchTextIndex += 1;
-    }
-  }
-
-  return searchTextIndex === searchText.length;
-}
-
-function inHours(hours, date) {
-  const d = date.getDay();
-  const h = date.getHours();
-  const m = date.getMinutes();
-  if (!hours[d]) {
-    return false;
-  }
-  const [ from, to ] = hours[d];
-  const x = (h * 60) + m;
-  return from === to ||
-    (from < to && from < x && x < to) ||
-    (from > to && !(to < x && x < from));
-}
-
-function populateDatalist($el, items) {
-  $el.append(items.map(item => $('<option></option>').text(item)));
 }
