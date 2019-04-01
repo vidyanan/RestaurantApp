5a9f7a6a5f54c215bbcae2743341d1f2c07a4eb4
ninjayoshi <tim470773@gmail.com>
2019-03-31 20:24:12 -0400

    Added basic routing functionality to userView.

--- client/js/userView.js
+++ client/js/userView.js
@@ -2,21 +2,18 @@ const close = document.querySelector("#closeWindow");
 close.addEventListener('click', closeWindow);
 
 async function getUserDetails(userId) {
-  let temp = {"id": 1, "firstName": "Tim", "lastName": "Cosby", "address": "Bahen", "email": "123@uoft", "phone": 905, "birthday": 0};
-  // Pull from server
-
-  return temp;
+  return $.get('/profile/' + userId);
 }
 
 async function fillInData(){
   let userId = 0;
   await getUserDetails(userId).then((dictionary) => {
-      $("#id").text(dictionary['id']);
+      $("#id").text(userId);
       $("#firstName").text(dictionary['firstName']);
       $("#lastName").text(dictionary['lastName']);
       $("#address").text(dictionary['address']);
       $("#email").text(dictionary['email']);
-      $("#phone").text(dictionary['phone']);
+      $("#phone").text(dictionary['phonenumber']);
       $("#birthday").text(dictionary['birthday']);
   });
 }
